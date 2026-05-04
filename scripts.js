a8f183def609f98dc5fb5e13569135644e247d4f
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-04 09:35:49 -0300

    Capturando o evento de input

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
-console.log('oi');
+//Seleciona os elementos do formulario.
+
+// pegando o input "valor da despesa"
+const amount = document.getElementById('amount');
+
+amount.oninput = () => {
+    
+};
