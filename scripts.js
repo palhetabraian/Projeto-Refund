9547958fc03ae52e20f82c218236437c385a6170
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-04 09:39:02 -0300

    Aceitando somente números no valor

--- scripts.js
+++ scripts.js
@@ -3,6 +3,10 @@
 // pegando o input "valor da despesa"
 const amount = document.getElementById('amount');
 
+//oninput propiedade que observa oque ta sendo digitado no input
 amount.oninput = () => {
-    
+    // Removendo a letra
+    let value = amount.value.replace(/\D/g, '');
+
+    amount.value = value;
 };
