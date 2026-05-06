2662ca669bdae641dc2da8f3ebc02e0d83a87a0f
Braian Nickolas Teixeira Palheta <134152507+palhetabraian@users.noreply.github.com>
2026-05-06 01:08:22 -0300

    Formatando o valor total

--- scripts.js
+++ scripts.js
@@ -118,7 +118,7 @@ function updateTotals() {
             const itemAmount = items[item].querySelector('.expense-amount');
 
             // Remover caracteres nao numericos e substui a virgula pelo ponto.
-            let value = itemAmount.textContent.replace(/[^\d]/g, '').replace(',', '.');
+            let value = itemAmount.textContent.replace(/[^\d,]/g, '').replace(',', '.');
 
             // Converte o valor para float.
             value = parseFloat(value);
@@ -132,8 +132,18 @@ function updateTotals() {
             total += Number(value);
         }
 
-        // Exibindo total
-        expensesTotal.textContent = total;
+        // Cria a span para adicionar o R$ formatado
+        const symbolBRL = document.createElement('small');
+        symbolBRL.textContent = 'R$';
+
+        // Formata o valor e remove o R$ que sera exibido pela small com um estilo customizado.
+        total = formatCurrencyBRL(total).toUpperCase().replace('R$', '');
+
+        // Limpa o conteudo do elemento.
+        expensesTotal.innerHTML = '';
+
+        // Adiciona o simbolo da moeda e o valor total formatado.
+        expensesTotal.append(symbolBRL, total);
     } catch (error) {
         console.log(error);
         alert('Nao foi possivel atualizar os totais.');
